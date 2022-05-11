51c789441ed80c992876c783a528818091b03c28
Adie Christopher <adiec7@gmail.com>
2022-05-11 21:45:57 +0100

    initial commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+
+    {
+        id: 1,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/51wH91YObNL._AC_SX184_.jpg',
+        title: 'The Lincoln Highway: A Novel.',
+        author: 'Amor Towles'
+    },
+    {
+        id: 42,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/41leOaqpoyL._AC._SR240,240.jpg',
+        title: 'Fanatical Prospecting: The Ultimate Guide to Opening Sales Conversations and Filling the Pipeline by Leveraging Social Selling, Telephone, Email, Text, and Cold Calling',
+        author: 'by Jeb Blount'
+    },
+    {
+        id: 3,
+        img: 'https://images-na.ssl-images-amazon.com/images/I/41r7b5RninL._AC_SX184_.jpg',
+        title: 'This Time Tomorrow: A Novel',
+        author: 'Emma Straub'
+    },
+]
